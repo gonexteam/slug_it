129cc57c1d60e6fd657f27f20baf5133d9c4cbcd
Truong Hung Huy <huyth12@cyberspace.vn>
2023-04-19 10:35:35 +0700

    fix - _vietnameseMap missing character

--- test/slug_it_test.dart
+++ test/slug_it_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:slug_it/slug_it.dart';
 
 void main() {
@@ -7,6 +6,7 @@ void main() {
     final slug = SlugIT();
     expect(slug.makeSlug('Haha... 2'), 'haha-2');
     expect(slug.makeSlug('Xin chào các bạn!'), 'xin-chao-cac-ban');
+    expect(slug.makeSlug('Hồ gươm'), 'ho-guom');
     expect(slug.makeSlug('How do i do with this project?', separator: '_'),
         'how_do_i_do_with_this_project');
   });
